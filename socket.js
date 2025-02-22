ea7d350d725c7b5a8a09cf914dd47a79156f1222
y1vonnef <yvonnef@alumni.cmu.edu>
2025-02-21 21:41:33 -0800

    Update socket.js

--- socket.js
+++ socket.js
@@ -2,4 +2,4 @@
 
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:3000/"); //makes connection to the port
+export const socket = io("wss://aiworldapp.yvonnefang1.repl.co"); //makes connection to the port
